5b2bfdbd9e8ae89e054e0824d1773d1718eb6be4
Jacob Joel <joezy28@gmail.com>
2022-05-24 23:21:43 +0530

    blk2

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -11,21 +11,22 @@ Clone the array before using sort method: [...arr]
 */
 
 // - Find the index of `101` in numbers
-
+numbers.indexOf(101);
 // - Find the last index of `9` in numbers
-
+numbers.lastIndexOf(9);
 // - Convert value of strings array into a sentance like "This is a collection of words"
-
+strings.join(` `);
 // - Add two new words in the strings array "called" and "sentance"
-
+strings.push(`called`, `sentance`);
 // - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
-
+strings.join(` `);
 // - Remove the first word in the array (strings)
-
+strings.shift();
 // - Find all the words that contain 'is' use string method 'includes'
-
+strings.includes(`is`);
 // - Find all the words that contain 'is' use string method 'indexOf'
-
+let final = [];
+strings.map((word) => final.push(word.indexOf(`is`)));
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
 
 // -  Sort Array from smallest to largest
